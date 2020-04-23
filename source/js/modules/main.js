85a03ff0f834bdf8f0dbd53a2ed278eefdeaf697
gorushkin <gorushkin@gmail.com>
2020-04-23 16:27:08 +0300

    доавил фичи в слайдер

--- source/js/modules/main.js
+++ source/js/modules/main.js
@@ -1,6 +1,39 @@
+const prevBtns = document.querySelectorAll('.card__arrow-btn--prev');
+const nextBtns = document.querySelectorAll('.card__arrow-btn--next');
+
+const disableBtns = (btnList) => {
+  console.log(btnList);
+  btnList.forEach((btn) => {
+    btn.classList.toggle('card__arrow-btn--disable');
+  });
+}
+
+const resetBtns = () => {
+  prevBtns.forEach((btn) => {
+    btn.classList.remove('card__arrow-btn--disable');
+  });
+  nextBtns.forEach((btn) => {
+    btn.classList.remove('card__arrow-btn--disable');
+  });
+}
+
 var headerSwiper = new Swiper('.header__swiper', {
   navigation: {
     nextEl: '.swiper--next',
     prevEl: '.swiper--prev',
+  },
+  on: {
+    init: function () {
+      disableBtns(prevBtns);
+    },
+    fromEdge: function () {
+      resetBtns();
+    },
+    reachBeginning: function () {
+      disableBtns(prevBtns);
+    },
+    reachEnd: function () {
+      disableBtns(nextBtns);
+    },
   },
 });
